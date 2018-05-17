5fe6dc2b0d521cbac0920a8da778691cf858028a
Vivek Kumar <kr.vivek.ab@gmail.com>
2018-05-17 16:39:59 +0530

    Create dataset.R
    Creating Rd files for dataset available in ``` animint2/data ``` folder. 
    
    TODO: add more dataset in this file, so error goes away! 
    
    Reason: To avoid the following error (during CRAN check)
    
    ```R 
     checking for missing documentation entries ... WARNING
    Undocumented code objects:
      'ChromHMMiterations' 'FluView' 'FunctionalPruning' 'PeakConsistency'
      'TestROC' 'UStornadoes' 'VariantModels' 'WorldBank' 'breakpoints'
      'change' 'climate' 'compare' 'generation.loci' 'intreg' 'malaria'
      'mixtureKNN' 'pirates' 'prior' 'prostateLasso' 'vervet' 'worldPop'
    Undocumented data sets:
      'ChromHMMiterations' 'FluView' 'FunctionalPruning' 'PeakConsistency'
      'TestROC' 'UStornadoes' 'VariantModels' 'WorldBank' 'breakpoints'
      'change' 'climate' 'compare' 'generation.loci' 'intreg' 'malaria'
      'mixtureKNN' 'pirates' 'prior' 'prostateLasso' 'vervet' 'worldPop'
    
    ```

---
+++ R/dataset.R
@@ -0,0 +1,97 @@
+##please note, this is not the correct documentation
+## this is just being done to avoid CRAN Warning for time being!
+
+##TODO: Proper description of each item listed in datasets. 
+
+
+#' List Data
+#'
+#' A Dataset containing list of 3 data frames in following format
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{metrics}{price in US dollars (\$326--\$18,823)}
+#'   \item{transition}{weight of the diamond (0.2--5.01)}
+#'   \item{emission}{quality of the cut (Fair, Good, Very Good, Premium, Ideal)}
+#' }
+"ChromHMMiterations"
+
+#' List Data
+#'
+#' A Dataset containing list of 3 data frames in following format
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{state_flu}{price in US dollars (\$326--\$18,823)}
+#'   \item{USpolygons}{weight of the diamond (0.2--5.01)}
+#' }
+"FluView"
+
+#' List Data
+#'
+#' A Dataset containing list of 3 data frames in following format
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{envelope}{price in US dollars (\$326--\$18,823)}
+#'   \item{cost.lines}{weight of the diamond (0.2--5.01)}
+#'   \item{minima}{quality of the cut (Fair, Good, Very Good, Premium, Ideal)}
+#'   \item{grid}{....}
+#' }
+"FunctionalPruning" 
+
+#' List Data
+#'
+#' A Dataset containing list of 3 data frames in following format
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{model}{...}
+#'   \item{truth}{..}
+#'   \item{signal}{....}
+#'   \item{guess}{...}
+#'   \item{increase}{...}
+#'   \item{error}{...}
+#'  }
+"PeakConsistency"
+
+#' List Data
+#'
+#' A Dataset containing list of 3 data frames in following format
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{fips}{price in US dollars (\$326--\$18,823)}
+#'   \item{ID}{weight of the diamond (0.2--5.01)}
+#'   \item{year}{....}
+#'   \item{month}{....}
+#'   \item{day}{....}
+#'   \item{date}{.....}
+#'   \item{time}{....}
+#'   \item{tz}{...}
+#'   \item{state}{...}
+#'   \item{state.tnum}{...}
+#'   \item{f}{...}
+#'   \item{injuries}{....}
+#'   \item{fatalities}{...}
+#'   \item{propertyLoss}{..}
+#'   \item{cropLoss}{...}
+#'   \item{startLat}{....}
+#'   \item{startLong}{...}
+#'   \item{endLat}{...}
+#'   \item{endLong}{...}
+#'   \item{trackLength}{....}
+#'   \item{trackWidth}{...}
+#'   \item{numStatesAffected}{....}
+#'   \item{stateNumber}{...}
+#'   \item{segmentNumber}{...}
+#'   \item{FipsCounty1}{...}
+#'   \item{FipsCounty2}{...}
+#'   \item{FipsCounty3}{...}
+#'   \item{FipsCounty4}{....}
+#'   \item{TotalPop2012}{....}
+#'   \item{LandArea}{...}
+#'   \item{TornadoesSqMile}{...}
+#'   \item{weight}{....}
+#' }
+"UStornadoes"
