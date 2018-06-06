d9e1029287fa898c220a4036f932e0d410ef8c11
adrianblanco <abrddj@gmail.com>
2018-06-05 21:02:37 -0400

    create get_alpha_stroke.R test

---
+++ tests/testthat/test-renderer1-get_alpha_stroke.R
@@ -0,0 +1,28 @@
+acontext("get_alpha_stroke")
+
+test_that("get_alpha_stroke in a viz", {
+
+  data(state)
+  states.data <- as.data.frame(state.x77)
+  states.data$states <- rownames(states.data)
+  colnames(states.data)[4] <- "Life.Exp"
+
+  viz <- ggplot(data = states.data,
+                aes(x = Income, y = Life.Exp)) +
+                geom_point(shape = 21,
+                            colour = "black",
+                            fill = "white",
+                            # I include it here to modify the stroke of geom_point
+                            # What do you think? Any advice? Is it a fraction the best way to set it?
+                            get_alpha_stroke = 1/10,
+                            validate_params = FALSE) +
+                ggtitle("Income v. Life Expectancy") +
+                theme(panel.background = element_rect(fill = NA),
+                      panel.grid.major = element_line(colour = "grey90")
+                )
+
+})
+
+# I am also developing a cartogram with ggplot2 to test get_aplha_stroke in the border of the states
+# I think it could be interesting as there are not tests or examples with maps. We can reuse it later for the new documentation
+# It should be ready tomorrow
