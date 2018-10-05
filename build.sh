44c98b93fc2295ae591e45061c5fd7cd96337fbc
Toby Dylan Hocking <tdhock5@gmail.com>
2018-10-05 11:15:42 -0700

    building with 4 NOTE

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ rm -rf animint2-release/tests/testthat/intreg-selection
 rm -rf animint2-release/tests/testthat/animint-htmltest
 rm -rf animint2-release/tests/testthat/ANIMINT_TEST_FOO
 rm -rf animint2-release/tests/testthat/test-compiler-gist.R
+rm -rf animint2-release/tests/testthat/.httr-oauth
 cat <<EOF > animint2-release/tests/testthat.R 
 library(testthat)
 test_check("animint2", filter="compiler")
