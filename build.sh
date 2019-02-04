d322b7f8c6f1696aa440ac8117e051e16e8c1db6
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2019-02-04 13:59:29 -0700

    windows build

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ cat <<EOF > animint2-release/tests/testthat.R
 library(testthat)
 test_check("animint2", filter="compiler")
 EOF
-PKG_TGZ=$(R CMD build animint2-release|grep building|sed 's/.*‘//'|sed 's/’.*//')
+PKG_TGZ=$(R CMD build animint2-release|grep building|sed "s/.*\(animint2.*.tar.gz\).*/\1/")
 R CMD INSTALL $PKG_TGZ
 R CMD check --as-cran $PKG_TGZ
