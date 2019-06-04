52555f0647481542273e4daa1ef52a6fe4f9b1b7
Toby Dylan Hocking <tdhock5@gmail.com>
2019-06-04 10:03:34 -0700

    ratio literal

--- tests/testthat/test-renderer1-facet-coord.R
+++ tests/testthat/test-renderer1-facet-coord.R
@@ -4,37 +4,35 @@ p <- ggplot(mtcars, aes(mpg, wt)) +
   geom_point(colour='grey50', size = 4) + 
   geom_point(aes(colour = cyl)) 
 
-test_that("coord_fixed() + facet_wrap(nrow=1) with shrunk y-axis", {
-  ratio2 <- 2
+test_that("coord_fixed(ratio=10) + facet_wrap(nrow=1) with shrunk y-axis", {
   wrapViz <-
     list(wrapPlot=p +
-         facet_wrap(~am) + coord_fixed(ratio2))
+         facet_wrap(~am) + coord_fixed(ratio=2))
   info <- animint2HTML(wrapViz)
   x.axes <- getNodeSet(info$html, "//g[contains(@class, 'xaxis')]")
   y.axes <- getNodeSet(info$html, "//g[contains(@class, 'yaxis')]")
   xdiff1 <- getTickDiff(x.axes[[1]])
   ydiff <- getTickDiff(y.axes[[1]], axis = "y")
-  diffs <- normDiffs(xdiff1, ydiff, ratio2)
+  diffs <- normDiffs(xdiff1, ydiff, 2)
   expect_equal(diffs[1], diffs[2], tolerance = 1, scale = 1)
   xdiff2 <- getTickDiff(x.axes[[2]])
-  diffs <- normDiffs(xdiff2, ydiff, ratio2)
+  diffs <- normDiffs(xdiff2, ydiff, 2)
   expect_equal(diffs[1], diffs[2], tolerance = 1, scale = 1)
 })
 
-test_that("coord_fixed() + facet_wrap(nrow=1) with shrunk x-axis", {
-  ratio10 <- 10
+test_that("coord_fixed(ratio=10) + facet_wrap(nrow=1) with shrunk x-axis", {
   wrapViz <-
     list(wrapPlot=p +
-           facet_wrap(~am) + coord_fixed(ratio10))
+           facet_wrap(~am) + coord_fixed(ratio=10))
   info <- animint2HTML(wrapViz)
   x.axes <- getNodeSet(info$html, "//g[contains(@class, 'xaxis')]")
   y.axes <- getNodeSet(info$html, "//g[contains(@class, 'yaxis')]")
   xdiff1 <- getTickDiff(x.axes[[1]])
   ydiff <- getTickDiff(y.axes[[1]], axis = "y")
-  diffs <- normDiffs(xdiff1, ydiff, ratio10)
+  diffs <- normDiffs(xdiff1, ydiff, 10)
   expect_equal(diffs[1], diffs[2], tolerance = 1, scale = 1)
   xdiff2 <- getTickDiff(x.axes[[2]])
-  diffs <- normDiffs(xdiff2, ydiff, ratio10)
+  diffs <- normDiffs(xdiff2, ydiff, 10)
   expect_equal(diffs[1], diffs[2], tolerance = 1, scale = 1)
 })
 
