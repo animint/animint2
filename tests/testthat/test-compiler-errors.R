2c12de55e70acd3b0576cfc02fdda95b4df29352
Toby Dylan Hocking <tdhock5@gmail.com>
2020-08-20 14:34:49 -0700

    expect list to avoid skip test

--- tests/testthat/test-compiler-errors.R
+++ tests/testthat/test-compiler-errors.R
@@ -34,6 +34,7 @@ test_that("no error for time option with clickSelects", {
     time=list(variable="Species", ms=3000)
   )
   info <- animint2dir(viz, open.browser=FALSE)
+  expect_is(info, "list")
 })
 
 test_that("no error for time option with showSelected", {
@@ -45,6 +46,7 @@ test_that("no error for time option with showSelected", {
     time=list(variable="Species", ms=3000)
   )
   info <- animint2dir(viz, open.browser=FALSE)
+  expect_is(info, "list")
 })
 
 test_that("no error for time option with color", {
@@ -55,6 +57,7 @@ test_that("no error for time option with color", {
     time=list(variable="Species", ms=3000)
   )
   info <- animint2dir(viz, open.browser=FALSE)
+  expect_is(info, "list")
 })
 
 data("WorldBank", package="animint2")
