9fbc6212112a724518022867e0c3ee89008a8b5f
Faye-yufan <594744538@qq.com>
2022-08-02 15:23:15 -0700

    Remove scale for color_off, fill_off for now

--- R/scale-gradient.r
+++ R/scale-gradient.r
@@ -2,9 +2,7 @@
 #'
 #' \code{scale_*_gradient} creates a two colour gradient (low-high),
 #' \code{scale_*_gradient2} creates a diverging colour gradient (low-mid-high),
-#' \code{scale_*_gradientn} creates an n-colour gradient.
-#'
-#' \code{scale_off_*} creates an aesthetic mapping for unselected points.
+#' \code{scale_*_gradientn} creats a n-colour gradient.
 #'
 #' Default colours are generated with \pkg{munsell} and
 #' \code{mnsl(c("2.5PB 2/4", "2.5PB 7/10")}. Generally, for continuous
@@ -13,7 +11,7 @@
 #' Munsell colour system.
 #'
 #' @inheritParams scales::seq_gradient_pal
-#' @inheritParams scale_hue
+#' @inheritParams scale_colour_hue
 #' @param low,high Colours for low and high ends of the gradient.
 #' @param guide Type of legend. Use \code{"colourbar"} for continuous
 #'   colour bar, or \code{"legend"} for discrete colour legend.
@@ -58,33 +56,35 @@
 #'   scale_colour_gradient(low = "white", high = "black")
 #' # Avoid red-green colour contrasts because ~10% of men have difficulty
 #' # seeing them
-scale_gradient <- function(aesthetic, ..., low = "#132B43", high = "#56B1F7", space = "Lab", na.value = "grey50", guide = "colourbar") {
-  continuous_scale(aesthetic, "gradient", seq_gradient_pal(low, high, space),
+scale_colour_gradient <- function(..., low = "#132B43", high = "#56B1F7", space = "Lab", na.value = "grey50", guide = "colourbar") {
+  continuous_scale("colour", "gradient", seq_gradient_pal(low, high, space),
     na.value = na.value, guide = guide, ...)
 }
 
 #' @rdname scale_gradient
 #' @export
-scale_colour_gradient <- function(...) {
-  scale_gradient("colour", ...)
+scale_fill_gradient <- function(..., low = "#132B43", high = "#56B1F7", space = "Lab", na.value = "grey50", guide = "colourbar") {
+  continuous_scale("fill", "gradient", seq_gradient_pal(low, high, space),
+    na.value = na.value, guide = guide, ...)
+}
+
+#' @inheritParams scales::div_gradient_pal
+#' @param midpoint The midpoint (in data value) of the diverging scale.
+#'   Defaults to 0.
+#' @rdname scale_gradient
+#' @export
+scale_colour_gradient2 <- function(..., low = muted("red"), mid = "white", high = muted("blue"), midpoint = 0, space = "Lab", na.value = "grey50", guide = "colourbar") {
+  continuous_scale("colour", "gradient2",
+    div_gradient_pal(low, mid, high, space), na.value = na.value, guide = guide, ...,
+    rescaler = mid_rescaler(mid = midpoint))
 }
 
 #' @rdname scale_gradient
 #' @export
-scale_colour_off_gradient <- function(...) {
-  scale_gradient("colour_off", ...)
-}
-
-#' @rdname scale_gradient
-#' @export
-scale_fill_gradient <- function(...) {
-  scale_gradient("fill", ...)
-}
-
-#' @rdname scale_gradient
-#' @export
-scale_fill_off_gradient <- function(...) {
-  scale_gradient("fill_off", ...)
+scale_fill_gradient2 <- function(..., low = muted("red"), mid = "white", high = muted("blue"), midpoint = 0, space = "Lab", na.value = "grey50", guide = "colourbar") {
+  continuous_scale("fill", "gradient2",
+    div_gradient_pal(low, mid, high, space), na.value = na.value, guide = guide, ...,
+    rescaler = mid_rescaler(mid = midpoint))
 }
 
 mid_rescaler <- function(mid) {
@@ -93,84 +93,21 @@ mid_rescaler <- function(mid) {
   }
 }
 
-#' @inheritParams scales::div_gradient_pal
-#' @param midpoint The midpoint (in data value) of the diverging scale.
-#'   Defaults to 0.
-#' @rdname scale_gradient
-#' @export
-scale_gradient2 <- function(aesthetic, ..., low = muted("red"), mid = "white", high = muted("blue"), midpoint = 0, space = "Lab", na.value = "grey50", guide = "colourbar") {
-  continuous_scale(aesthetic, "gradient2",
-    div_gradient_pal(low, mid, high, space), na.value = na.value, guide = guide, ...,
-    rescaler = mid_rescaler(mid = midpoint))
-}
-
-#' @inheritParams scales::div_gradient_pal
-#' @param midpoint The midpoint (in data value) of the diverging scale.
-#'   Defaults to 0.
-#' @rdname scale_gradient
-#' @export
-scale_colour_gradient2 <- function(...) {
-  scale_gradient2("colour", ...)
-}
-
-#' @inheritParams scales::div_gradient_pal
-#' @param midpoint The midpoint (in data value) of the diverging scale.
-#'   Defaults to 0.
-#' @rdname scale_gradient
-#' @export
-scale_colour_off_gradient2 <- function(...) {
-  scale_gradient2("colour_off", ...)
-}
-
-#' @inheritParams scales::div_gradient_pal
-#' @param midpoint The midpoint (in data value) of the diverging scale.
-#'   Defaults to 0.
-#' @rdname scale_gradient
-#' @export
-scale_fill_gradient2 <- function(...) {
-  scale_gradient2("fill", ...)
-}
-
-#' @inheritParams scales::div_gradient_pal
-#' @param midpoint The midpoint (in data value) of the diverging scale.
-#'   Defaults to 0.
-#' @rdname scale_gradient
-#' @export
-scale_fill_off_gradient2 <- function(...) {
-  scale_gradient2("fill_off", ...)
-}
-
 #' @inheritParams scales::gradient_n_pal
 #' @param colours,colors Vector of colours to use for n-colour gradient.
 #' @rdname scale_gradient
 #' @export
-scale_gradientn <- function(aesthetic, ..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar", colors) {
+scale_colour_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar", colors) {
   colours <- if (missing(colours)) colors else colours
 
-  continuous_scale(aesthetic, "gradientn",
+  continuous_scale("colour", "gradientn",
     gradient_n_pal(colours, values, space), na.value = na.value, guide = guide, ...)
 }
-
 #' @rdname scale_gradient
 #' @export
-scale_colour_gradientn <- function(...) {
-  scale_gradientn("colour", ...)
-}
+scale_fill_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar", colors) {
+  colours <- if (missing(colours)) colors else colours
 
-#' @rdname scale_gradient
-#' @export
-scale_colour_off_gradientn <- function(...) {
-  scale_gradientn("colour_off", ...)
-}
-
-#' @rdname scale_gradient
-#' @export
-scale_fill_gradientn <- function(...) {
-  scale_gradientn("fill", ...)
-}
-
-#' @rdname scale_gradient
-#' @export
-scale_fill_off_gradientn <- function(...) {
-  scale_gradientn("fill_off", ...)
+  continuous_scale("fill", "gradientn",
+    gradient_n_pal(colours, values, space), na.value = na.value, guide = guide, ...)
 }
