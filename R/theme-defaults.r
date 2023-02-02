8dc3bb39f4d6b174d015e99e5e72fd1d002381a6
Toby Dylan Hocking <tdhock5@gmail.com>
2023-02-02 04:46:25 -0700

    theme_bw no longer overwrites axis.text size

--- R/theme-defaults.r
+++ R/theme-defaults.r
@@ -154,7 +154,6 @@ theme_bw <- function(base_size = 12, base_family = "") {
   # Starts with theme_grey and then modify some parts
   theme_grey(base_size = base_size, base_family = base_family) %+replace%
     theme(
-      axis.text         = element_text(size = rel(0.8)),
       axis.ticks        = element_line(colour = "black"),
       legend.key        = element_rect(colour = "grey80"),
       panel.background  = element_rect(fill = "white", colour = NA),
