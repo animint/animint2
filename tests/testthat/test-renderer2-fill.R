1040aa9dd8d6070838f783be28b9eb8454da0211
Faye-yufan <yufanfei@usc.edu>
2023-09-07 23:31:07 -0700

    Modify fill_off tests for better readability

--- tests/testthat/test-renderer2-fill.R
+++ tests/testthat/test-renderer2-fill.R
@@ -1,57 +1,42 @@
 acontext("fill_off")
 
 #
-# test geom with both fill and colour styles
+# Test geoms with both fill and colour styles
 #
-viz.point <- list(pointone = ggplot() + geom_point(
-  data = mtcars,
-  size = 10,
-  aes(x=wt, y=mpg,
-      colour = disp),
-  clickSelects = "gear")+
-  ggtitle("default alpha_off(0.5) style"),
+common_aes <- aes(x=wt, y=mpg, fill = disp)
 
-pointtwo = ggplot() + geom_point(
-  data = mtcars,
-  fill_off="transparent",
-  size = 10,
-  aes(x=wt, y=mpg,
-      fill = disp),
-  clickSelects = "gear")+
-  ggtitle("colour corresponding to `disp` group, fill_off=\"transparent\" "),
+viz_point <- list(
+  default_alpha_off = ggplot() + geom_point(data = mtcars, size = 10, aes = common_aes, clickSelects = "gear") +
+                      ggtitle("default alpha_off(0.5) style"),
+  
+  fill_off_specified = ggplot() + geom_point(data = mtcars, fill_off = "transparent", size = 10, aes = common_aes, clickSelects = "gear") +
+                        ggtitle("Fill set to transparent when not selected"),
+  
+  fill_and_alpha_off = ggplot() + geom_point(data = mtcars, alpha_off = 0.5, fill_off = "grey", size = 10, aes = common_aes, clickSelects = "gear") +
+                        ggtitle("Both fill and alpha change when selected")
+)
 
-pointthree = ggplot() + geom_point(
-  data = mtcars,
-  alpha_off=0.5,
-  fill_off="grey",
-  size = 10,
-  aes(x=wt, y=mpg,
-      fill = disp,
-      id=paste0("pointthree_disp", disp, "gear", gear, "wt", wt)),
-  clickSelects = "gear")+
-  ggtitle("fill_off + alpha_off"))
-
-info2 <- animint2HTML(viz.point)
+viz_info <- animint2HTML(viz.point)
 
 test_that("fill_off only changes fill when clicked, colour does not change", {
-  point.xpath <- '//svg[@id="plot_pointthree"]//circle[@id="pointthree_disp275.8gear3wt3.73"]'
-  circle.list <- getNodeSet(info2$html, point.xpath)
-  before.click.color <- getStyleValue(info2$html, point.xpath, "stroke")
-  before.click.fill <- getStyleValue(info2$html, point.xpath, "fill")
+  point_xpath <- '//svg[@id="plot_fill_and_alpha_off"]//circle[@id="fill_and_alpha_off_disp275.8gear3wt3.73"]'
+  circle_list <- getNodeSet(viz_info$html, point_xpath)
+  before_click_color <- getStyleValue(viz_info$html, point_xpath, "stroke")
+  before_click_fill <- getStyleValue(viz_info$html, point_xpath, "fill")
   
-  clickID('pointthree_disp275.8gear3wt3.73')
+  clickID('fill_and_alpha_off_disp275.8gear3wt3.73')
   html <- getHTML()
-  after.click.color <- getStyleValue(html, point.xpath, "stroke")
-  after.click.fill <- getStyleValue(html, point.xpath, "fill")
+  after_click_color <- getStyleValue(html, point_xpath, "stroke")
+  after_click_fill <- getStyleValue(html, point_xpath, "fill")
   
-  expect_false(isTRUE(all.equal(before.click.fill, after.click.fill)))
-  expect_color(after.click.color, before.click.color)
+  expect_false(isTRUE(all.equal(before_click_fill, after_click_fill)))
+  expect_color(after_click_color, before_click_color)
 })
 
-test_that("fill and color are not same", {
-  point.xpath <- '//svg[@id="plot_pointthree"]//circle[@class="geom"]'
-  circle.list <- getNodeSet(info2$html, point.xpath)
-  circle.color <- getStyleValue(info2$html, point.xpath, "stroke")
-  circle.fill <- getStyleValue(info2$html, point.xpath, "fill")
-  expect_false(isTRUE(all.equal(circle.color, circle.fill)))
+test_that("fill and color are not the same", {
+  point_xpath <- '//svg[@id="plot_fill_and_alpha_off"]//circle[@class="geom"]'
+  circle_list <- getNodeSet(viz_info$html, point_xpath)
+  circle_color <- getStyleValue(viz_info$html, point_xpath, "stroke")
+  circle_fill <- getStyleValue(viz_info$html, point_xpath, "fill")
+  expect_false(isTRUE(all.equal(circle_color, circle_fill)))
 })
