1d888e3fe55e3cec7db35d40afc81c0928b9d278
Yufan Fei <62975717+Faye-yufan@users.noreply.github.com>
2023-09-12 10:14:10 -0700

    Add error message for missing GitHub token to include detailed setup instructions

--- R/z_pages.R
+++ R/z_pages.R
@@ -24,6 +24,21 @@
 #'
 #' @export
 animint2pages <- function(plot.list, github_repo, commit_message = "Commit from animint2pages", private = FALSE, ...) {
+  # Check for GitHub token
+  github_token <- Sys.getenv("GITHUB_PAT")
+  if (identical(github_token, "")) {
+    stop("A GitHub token is required to create and push to a new repository. \n",
+     "To create a GitHub token, follow these steps:\n",
+     "1. Go to https://github.com/settings/tokens/new?scopes=repo&description=animint2pages\n",
+     "2. Confirm your password if prompted.\n",
+     "3. Ensure that the 'repo' scope is checked.\n",
+     "4. Click 'Generate token' at the bottom of the page.\n",
+     "5. Copy the generated token.\n",
+     "After creating the token, you can set it up in your R environment by running: \n",
+     "gitcreds::gitcreds_set()\n",
+     "And then paste the token when prompted.")
+  }
+  
   # Check for required packages
   if (!requireNamespace("gert")) {
     stop("Please run `install.packages('gert')` before using this function")
