717cab6afeab64e3f6d5257001134b2fd6bc711f
Toby Dylan Hocking <toby.hocking@r-project.org>
2023-11-01 22:44:26 -0700

    fix pages tests

--- R/z_pages.R
+++ R/z_pages.R
@@ -6,10 +6,15 @@
 #' Before using this function set your appropriate git 'user.username' and 'user.email'
 #'
 #' @param plot.list A named list of ggplots and option lists.
-#' @param github_repo The name of the GitHub repository to which the files will be pushed.
-#' @param commit_message A string specifying the commit message for the pushed files.
-#' @param private A logical flag indicating whether the GitHub repository should be private or not.
-#' @param required_opts Character vector of plot.list element names which are checked (stop with an error if not present). Use required_opts=NULL to skip check.
+#' @param github_repo The name of the GitHub repository to which the
+#'   files will be pushed.
+#' @param commit_message A string specifying the commit message for
+#'   the pushed files.
+#' @param private A logical flag indicating whether the GitHub
+#'   repository should be private or not (default FALSE).
+#' @param required_opts Character vector of plot.list element names
+#'   which are checked (stop with an error if not present). Use
+#'   required_opts=NULL to skip check.
 #' @param ... Additional options passed onto \code{animint2dir}.
 #'
 #' @return The function returns the initialized GitHub repository object.
@@ -31,54 +36,45 @@
 #'
 #' @export
 animint2pages <- function(plot.list, github_repo, commit_message = "Commit from animint2pages", private = FALSE, required_opts = c("title","source"), ...) {
-
   for(opt in required_opts){
     if(!opt %in% names(plot.list)){
       stop(sprintf("plot.list does not contain option named %s, which is required by animint2pages", opt))
     }
   }
-
   # Check for required packages
   for(pkg in c("gert", "gh")){
     if (!requireNamespace(pkg)) {
       stop(sprintf("Please run `install.packages('%s')` before using this function", pkg))
     }
   }
-
   # Generate plot files
   res <- animint2dir(plot.list, open.browser = FALSE, ...)
-
   # Select non-ignored files to post
   all_files <- Sys.glob(file.path(res$out.dir, "*"))
   file_info <- file.info(all_files)
   to_post <- all_files[!(file_info$size == 0 | grepl("~$", all_files))]
-
-  tmp_dir <- tempfile()
-
   tryCatch({
-    creds <- gitcreds::gitcreds_get()
+    gitcreds::gitcreds_get()
   }, error = function(e) stop("A GitHub token is required to create and push to a new repository. \nTo create a GitHub token, follow these steps:\n1. Go to https://github.com/settings/tokens/new?scopes=repo&description=animint2pages\n2. Confirm your password if prompted.\n3. Ensure that the 'repo' scope is checked.\n4. Click 'Generate token' at the bottom of the page.\n5. Copy the generated token.\nAfter creating the token, you can set it up in your R environment by running: \nSys.setenv(GITHUB_PAT=\"yourGithubPAT\") \ngert::git_config_global_set(\"user.name\", \"yourUserName\") \ngert::git_config_global_set(\"user.email\", \"yourEmail\") \n"))
-
   # Raise error if github_repo contains '/'
   if (grepl("/", github_repo)) {
     stop("The github_repo argument should not contain '/'.")
   }
-  
   # Check for existing repository
   whoami <- suppressMessages(gh::gh_whoami())
-  owner <- whoami$login
-  
+  owner <- whoami[["login"]]
+  viz_owner_repo <- paste0(owner, "/", github_repo)
+  local_clone <- tempfile()
   if (!check_no_github_repo(owner, github_repo)) {
     create <- gh::gh("POST /user/repos", name = github_repo, private = private)
     origin_url <- create$clone_url
-    repo <- gert::git_init(path = tmp_dir)
+    repo <- gert::git_init(path = local_clone)
     gert::git_remote_add(name = "origin", url = origin_url, repo = repo)
   } else {
-    origin_url <- paste0("https://github.com/", owner, "/", github_repo, ".git")
-    repo <- gert::git_clone(origin_url, tmp_dir)
+    origin_url <- paste0("https://github.com/", viz_owner_repo, ".git")
+    repo <- gert::git_clone(origin_url, local_clone)
   }
-
-  viz_url <- paste0("https://", whoami$login, ".github.io/", github_repo)
+  viz_url <- paste0("https://", owner, ".github.io/", github_repo)
   # check if repo has commit, if not, give it first commit, this can avoid error
   has_commits <- FALSE
   try(
@@ -90,26 +86,21 @@ animint2pages <- function(plot.list, github_repo, commit_message = "Commit from 
     silent = TRUE
   )
   if (!has_commits) {
-    initial_commit(tmp_dir, repo, viz_url)
+    initial_commit(local_clone, repo, viz_url)
   }
-
   # Handle gh-pages branch
-  manage_gh_pages(repo, to_post, tmp_dir, commit_message)
-  message(
-    "Visualization will be available at ", viz_url,
-    "\nDeployment via GitHub Pages may take a few minutes..."
-  )
-
-  repo
+  manage_gh_pages(repo, to_post, local_clone, commit_message)
+  message(sprintf(
+    "Visualization will be available at %s\nDeployment via GitHub Pages may take a few minutes...", viz_url))
+  viz_owner_repo
 }
 
-initial_commit <- function(tmp_dir, repo, viz_url) {
-  readme_file_path <- file.path(tmp_dir, "README.md")
+initial_commit <- function(local_clone, repo, viz_url) {
+  readme_file_path <- file.path(local_clone, "README.md")
   header <- "## New animint visualization\n"
   url_hyperlink <- sprintf("[%s](%s)\n", viz_url, viz_url)
   full_content <- paste0(header, url_hyperlink)
   writeLines(full_content, readme_file_path)
-
   gert::git_add("README.md", repo = repo)
   gert::git_commit("Initial commit", repo = repo)
   df_or_vec <- gert::git_branch(repo)
@@ -128,15 +119,13 @@ initial_commit <- function(tmp_dir, repo, viz_url) {
   gert::git_push(repo = repo, remote = "origin", set_upstream = TRUE)
 }
 
-manage_gh_pages <- function(repo, to_post, tmp_dir, commit_message) {
+manage_gh_pages <- function(repo, to_post, local_clone, commit_message) {
   branches <- gert::git_branch_list(local = TRUE, repo = repo)
-
   if (!"gh-pages" %in% branches$name) {
     gert::git_branch_create(repo = repo, branch = "gh-pages")
   }
-
   gert::git_branch_checkout("gh-pages", repo = repo)
-  file.copy(to_post, tmp_dir, recursive = TRUE)
+  file.copy(to_post, local_clone, recursive = TRUE)
   gert::git_add(files = ".", repo = repo)
   gert::git_commit(message = commit_message, repo = repo)
   gert::git_push(remote = "origin", set_upstream = TRUE, repo = repo, force = TRUE)
@@ -152,20 +141,20 @@ check_no_github_repo <- function(owner, repo) {
   )
 }
 
-get_pages_info <- function(viz_user_repo){
+get_pages_info <- function(viz_owner_repo){
   viz_dir <- tempfile()
-  origin_url <- paste0("https://github.com/", viz_user_repo, ".git")
+  origin_url <- paste0("https://github.com/", viz_owner_repo, ".git")
   gert::git_clone(origin_url, viz_dir)
   gert::git_branch_checkout("gh-pages", repo=viz_dir)
   Capture.PNG <- file.path(viz_dir, "Capture.PNG")
   if(!file.exists(Capture.PNG)){
-    stop(sprintf("gh-pages branch of %s should contain file named Capture.PNG (screenshot of data viz)", viz_user_repo))
+    stop(sprintf("gh-pages branch of %s should contain file named Capture.PNG (screenshot of data viz)", viz_owner_repo))
   }
   plot.json <- file.path(viz_dir, "plot.json")
   jlist <- RJSONIO::fromJSON(plot.json)
   commit.row <- gert::git_log(max=1, repo=viz_dir)
   repo.row <- data.table(
-    viz_user_repo, Capture.PNG, commit.POSIXct=commit.row$time)
+    viz_owner_repo, Capture.PNG, commit.POSIXct=commit.row$time)
   to.check <- c(
     source="URL of data viz source code",
     title="string describing the data viz")
@@ -179,7 +168,7 @@ get_pages_info <- function(viz_user_repo){
     ){
       set(repo.row, j=attr.name, value=attr.value)
     }else{
-      stop(sprintf("plot.json file in gh-pages branch of %s should have element named %s which should be %s", viz_user_repo, attr.name, to.check[[attr.name]]))
+      stop(sprintf("plot.json file in gh-pages branch of %s should have element named %s which should be %s", viz_owner_repo, attr.name, to.check[[attr.name]]))
     }
   }
   repo.row
@@ -201,26 +190,26 @@ get_pages_info <- function(viz_user_repo){
 ##' @export
 update_gallery <- function(gallery_path="~/R/gallery"){
   repos.txt <- file.path(gallery_path, "repos.txt")
-  repos.dt <- fread(repos.txt,header=FALSE,col.names="viz_user_repo")
+  repos.dt <- fread(repos.txt,header=FALSE,col.names="viz_owner_repo")
   meta.csv <- file.path(gallery_path, "meta.csv")
   old.meta <- fread(meta.csv)
-  todo.meta <- repos.dt[!old.meta, on="viz_user_repo"]
+  todo.meta <- repos.dt[!old.meta, on="viz_owner_repo"]
   meta.dt.list <- list(old.meta)
   error.dt.list <- list()
   add.POSIXct <- Sys.time()
-  for(viz_user_repo in todo.meta[["viz_user_repo"]]){
+  for(viz_owner_repo in todo.meta[["viz_owner_repo"]]){
     tryCatch({
-      meta.row <- data.table(add.POSIXct, get_pages_info(viz_user_repo))
-      meta.dt.list[[viz_user_repo]] <- meta.row
+      meta.row <- data.table(add.POSIXct, get_pages_info(viz_owner_repo))
+      meta.dt.list[[viz_owner_repo]] <- meta.row
       Capture.PNG <- meta.row[["Capture.PNG"]]
       repo.png <- file.path(
-        gallery_path, "repos", paste0(viz_user_repo, ".png"))
+        gallery_path, "repos", paste0(viz_owner_repo, ".png"))
       user.dir <- dirname(repo.png)
       dir.create(user.dir, showWarnings = FALSE, recursive = TRUE)
       file.copy(Capture.PNG, repo.png, overwrite = TRUE)
     }, error=function(e){
-      error.dt.list[[viz_user_repo]] <<- data.table(
-        add.POSIXct, viz_user_repo, error=e$message)
+      error.dt.list[[viz_owner_repo]] <<- data.table(
+        add.POSIXct, viz_owner_repo, error=e$message)
     })
   }
   (meta.dt <- rbindlist(meta.dt.list))
--- tests/testthat/test-compiler-ghpages.R
+++ tests/testthat/test-compiler-ghpages.R
@@ -1,31 +1,47 @@
 acontext("GitHub Pages")
 
-Sys.setenv(GITHUB_PAT = Sys.getenv("GITHUB_PAT"))
+viz <- animint(
+  title="one to ten",
+  source="https://github.com/animint/animint2/tree/master/tests/testthat/test-compiler-ghpages.R",
+  p=ggplot(data.frame(x = 1:10, y = 1:10), aes(x, y)) +
+    geom_point())
 
-plot <- ggplot(data.frame(x = 1:10, y = 1:10), aes(x, y)) +
-  geom_point()
+test_that("error for viz with no title", {
+  viz.no.title <- viz
+  viz.no.title$title <- NULL
+  expect_error({
+    animint2pages(viz.no.title, "no-title")
+  }, "plot.list does not contain option named title, which is required by animint2pages")
+})
 
-test_that("animint2pages() returns an object of class 'git_repository'", {
-  repo <- animint2pages(list(p = plot), github_repo = "test_repo")
-  expect_is(repo, "git_repository")
+test_that("error for viz with no source", {
+  viz.no.source <- viz
+  viz.no.source$source <- NULL
+  expect_error({
+    animint2pages(viz.no.source, "no-source")
+  }, "plot.list does not contain option named source, which is required by animint2pages")
+})
+
+viz_owner_repo <- animint2pages(viz, github_repo = "animint2pages_test_repo")
+test_that("animint2pages() returns owner/repo string", {
+  expect_is(viz_owner_repo, "character")
 })
 
 test_that("animint2pages raises an error if no GitHub token is present", {
-  withr::local_envvar(c(GITHUB_PAT = NULL), {
-    expect_error(
-      animint2pages(list(p = plot), github_repo = "test_repo"),
-      paste0(
-        "A GitHub token is required to create and push to a new repository. \n",
-        "To create a GitHub token, follow these steps:\n",
-        "1. Go to https://github.com/settings/tokens/new?scopes=repo&description=animint2pages\n",
-        "2. Confirm your password if prompted.\n",
-        "3. Ensure that the 'repo' scope is checked.\n",
-        "4. Click 'Generate token' at the bottom of the page.\n",
-        "5. Copy the generated token.\n",
-        "After creating the token, you can set it up in your R environment by running: \n",
-        "gitcreds::gitcreds_set()\n",
-        "And then paste the token when prompted."
-      )
-    )
-  })
+  env.names <- c("GITHUB_PAT", "GITHUB_PAT_GITHUB_COM")
+  env.old <- Sys.getenv(env.names)
+  Sys.unsetenv(env.names)
+  ## removing env vars is necessary but not sufficient for this test,
+  ## because if they do not exist, then gitcreds::gitcreds_get() will
+  ## be called to set the env vars/token.
+  repo.root <- system("git rev-parse --show-toplevel", intern=TRUE)
+  config.file <- file.path(repo.root, ".git", "config")
+  config.old <- file.path(repo.root, ".git", "config.old")
+  file.copy(config.file, config.old, overwrite = TRUE)
+  cat("[credential]\n\tusername = FOO", file=config.file, append=TRUE)
+  expect_error({
+    animint2pages(viz, github_repo = "test_repo")
+  }, "A GitHub token is required to create and push to a new repository. \nTo create a GitHub token, follow these steps:\n1. Go to https://github.com/settings/tokens/new?scopes=repo&description=animint2pages\n2. Confirm your password if prompted.\n3. Ensure that the 'repo' scope is checked.\n4. Click 'Generate token' at the bottom of the page.\n5. Copy the generated token.\nAfter creating the token, you can set it up in your R environment by running: \nSys.setenv(GITHUB_PAT=\"yourGithubPAT\") \ngert::git_config_global_set(\"user.name\", \"yourUserName\") \ngert::git_config_global_set(\"user.email\", \"yourEmail\") \n", fixed=TRUE)
+  do.call(Sys.setenv, as.list(env.old))
+  file.copy(config.old, config.file, overwrite = TRUE)
 })
