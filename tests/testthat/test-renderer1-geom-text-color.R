cbdabd1401644bb9b60616d6dee33ebfd32dee8e
Toby Dylan Hocking <toby.hocking@r-project.org>
2023-11-04 23:03:25 -0700

    text color tests

---
+++ tests/testthat/test-renderer1-geom-text-color.R
@@ -0,0 +1,24 @@
+acontext("geom text color")
+
+df <- data.frame(x=1,y="foo")
+viz <- animint(
+  text=ggplot()+
+    geom_text(aes(x, 4, label=y), color="black", clickSelects="y", data=df)+
+    geom_text(aes(x, 3, label=y, color=y), data=df)+
+    scale_color_manual(values=c(foo="blue"))+
+    geom_text(aes(x, 2, label=y), color="red", data=df)+
+    geom_text(aes(x, 1, label=y), color="black", color_off="pink", clickSelects="y", data=df))
+info <- animint2HTML(viz)
+test_that("geom_text color rendered as fill style", {
+  fill <- getStyleValue(info$html, '//text[@class="geom"]', "fill")
+  expect_color(fill, c("black", "blue","red","black"))
+  opacity <- getStyleValue(info$html, '//text[@class="geom"]', "opacity")
+  expect_identical(opacity, c("1","1","1","1"))
+})
+clickID("plot_text_y_variable_foo_svg")
+test_that("geom_text color rendered as fill style", {
+  fill <- getStyleValue(info$html, '//text[@class="geom"]', "fill")
+  expect_color(fill, c("black", "red","pink"))
+  opacity <- getStyleValue(info$html, '//text[@class="geom"]', "opacity")
+  expect_identical(opacity, c("0.5","1","1"))
+})
