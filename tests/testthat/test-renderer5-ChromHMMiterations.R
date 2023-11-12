b4eed26622cf44b9ba7fd685dd75c0c0472ed500
Toby Dylan Hocking <toby.hocking@r-project.org>
2023-11-12 16:10:45 -0700

    fix scales

--- tests/testthat/test-renderer5-ChromHMMiterations.R
+++ tests/testthat/test-renderer5-ChromHMMiterations.R
@@ -5,40 +5,47 @@ data(ChromHMMiterations, package = "animint2")
 emission <- data.frame(ChromHMMiterations$emission, parameters="emission")
 transition <- data.frame(ChromHMMiterations$transition, parameters="transition")
 
+unique(transition$state.from)
+transition$state0.to <- sprintf("%02d", transition$state.to)
+emission$exp.fac <- factor(emission$experiment, unique(emission$experiment))
 viz <- list(
   parameters=ggplot()+
     ggtitle("parameters at selected iteration")+
     scale_fill_gradient(low="white", high="blue")+
-    geom_tile(aes(state, experiment, fill=frequency,
-                  key=paste(state, experiment)),
-              color="black",
-              showSelected="iteration",
-              data=emission)+
+    scale_x_discrete("State coming from")+
+    scale_y_discrete("", drop=TRUE)+
+    geom_tile(aes(
+      state, exp.fac, fill=frequency,
+      key=paste(state, experiment)),
+      showSelected="iteration",
+      data=emission)+
     scale_color_gradient(low="white", high="red")+
     theme_bw()+
-    theme_animint(height=600, width=350)+
+    theme_animint(height=500, width=350)+
     theme(panel.margin=grid::unit(0, "cm"))+
-    facet_grid(parameters ~ .,
-               space="free",
-               scales="free_y")+
-    scale_y_discrete(drop=FALSE)+
-    geom_point(aes(state.to, state.from, color=probability,
-                  key=paste(state.from, state.to)),
-               showSelected="iteration",
-               size=10,
-               data=transition),
+    facet_grid(
+      parameters ~ .,
+      space="free",
+      scales="free_y")+
+    geom_point(aes(
+      state.from, state0.to, color=probability,
+      key=paste(state.from, state.to)),
+      showSelected="iteration",
+      size=10,
+      data=transition),
   metrics=ggplot()+
     ggtitle("convergence metrics, select iteration")+
     make_tallrect(ChromHMMiterations$metrics, "iteration")+
-    geom_line(aes(iteration, metric.value),
-              data=ChromHMMiterations$metrics)+
+    geom_line(aes(
+      iteration, metric.value),
+      data=ChromHMMiterations$metrics)+
     theme_bw()+
     theme_animint(height=500)+
-    theme(panel.margin=grid::unit(0, "cm"))+
     facet_grid(metric.name ~ ., scales="free_y"),
   duration=list(iteration=500),
   first=list(iteration=100),
   title="ChromHMM parameter fitting for one iPS sample")
+viz$param
 
 expect_no_warning({
   info <- animint2HTML(viz)
