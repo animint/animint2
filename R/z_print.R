6305a3be0851d28e8e4b493eb819d9fe4070684b
Siddhesh Deodhar <siddheshdeodhar95@gmail.com>
2024-02-07 00:34:03 -0500

    Update error message

--- R/z_print.R
+++ R/z_print.R
@@ -85,7 +85,7 @@ animint <- function(...){
   
   # Check if argument list is empty
   if(length(L) == 0) {
-    stop(paste("No arguments passed to animint"))
+    stop("No arguments passed to animint. Arguments should 1 or more include ggplot and 0 or more options")
   }
   
   # Generate a list of duplicate named arguments
