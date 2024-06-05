54447b1295ce396a99a7aa82fd8329ef65e74c43
SiddheshDeodhar <153800103+siddhesh195@users.noreply.github.com>
2024-06-05 10:53:13 -0400

    Update test-renderer1-global-variables.R

--- tests/testthat/test-renderer1-global-variables.R
+++ tests/testthat/test-renderer1-global-variables.R
@@ -12,26 +12,49 @@ for(var b in window) {
 }
 return myArray;'
 
+myScript2 <- 'myArray = [];
+for(var b in window) { 
+  if(window.hasOwnProperty(b)) {myArray.push(b);} 
+}'
+
 getVariables <- function(){
-  vars <- remDr$executeScript(myScript)
+  if (remDr$browserName=="chromote"){
+    
+    vars <- remDr$Runtime$evaluate(myScript2,returnByValue = FALSE)
+    print(vars)
+  } else {
+    vars <- remDr$executeScript(myScript)
+  }  
   # ignore the "plot" variable -- 
   # https://github.com/tdhock/animint/pull/62#issuecomment-100008532
   # also ignore jQuery1238915281937 variable:
   grep("plot|jQuery", vars, value=TRUE, invert=TRUE)
 }
 
+    
+   
+    
+  
+  
+
+
 test_that("animint.js only defines 1 object, called animint", {
   info <- animint2HTML(viz)
   animint.vars <- getVariables()
+  #print(animint.vars)
   index.file <- file.path("animint-htmltest", "index.html")
+  #print(index.file)
   html.lines <- readLines(index.file)
+  #print(html.lines)
   html.without <- html.lines[!grepl("animint.js", html.lines)]
+  #print(html.without)
   cat(html.without, file=index.file, sep="\n")
   # Note: It's OK for the webdriver to spit out 
   # ReferenceError: Can't find variable: animint
   # since we've removed the animint.js script
   remDr$refresh()
   without.vars <- getVariables()
+  #print(without.vars)
   diff.vars <- animint.vars[!animint.vars %in% without.vars]
   print(diff.vars)
   expect_identical(diff.vars, "animint")
