6872e8d61edcfcb1f1618539ba04157e85fabb28
siddhesh195 <siddheshdeodhar95@gmail.com>
2024-06-24 02:49:34 -0400

    corrected hardcoded repo name to actual repo name during the test
    corrected hardcoded repo name to actual repo name during the test

--- tests/testthat/test-compiler-ghpages.R
+++ tests/testthat/test-compiler-ghpages.R
@@ -32,9 +32,10 @@ test_that("check if animint2pages() successfully uploads screenshot", {
   viz_owner_repo <- animint2pages(viz, github_repo = "animint2pages_test_repo")
   split_viz_owner_repo <- strsplit(viz_owner_repo, "/")[[1]]
   repo_owner <- split_viz_owner_repo[1]
+  repo_name <- split_viz_owner_repo[2]
   file_exists <- tryCatch({
     gh("GET /repos/:owner/:repo/contents/:path",
-       owner = repo_owner, repo = "animint2pages_test_repo", path = "screenshot.png",ref = "gh-pages")
+       owner = repo_owner, repo = repo_name, path = "screenshot.png",ref = "gh-pages")
     TRUE  # If the call succeeds, the file exists
   }, error = function(e) {
     FALSE  # If an error occurs, assume the file does not exist
