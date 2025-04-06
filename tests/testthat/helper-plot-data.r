b78ce9647df619d5aaf7c094bb9a9521d4dc406f
Suhaani Agarwal <suhaani0707@gmail.com>
2025-04-06 12:57:56 +0530

    changed .BY[[1]]

--- tests/testthat/helper-plot-data.r
+++ tests/testthat/helper-plot-data.r
@@ -1,17 +1,14 @@
 library(data.table)
 # Transform the data as the coordinate system does
-
 cdata <- function(plot) {
   pieces <- ggplot_build(plot)
-  
   # Process each piece of data while maintaining panel structure
   result <- lapply(pieces$data, function(d) {
     dt <- as.data.table(d)
     
     # Explicitly group by PANEL and process each panel's data
     dt[, {
-      current_panel <- .BY[[1]]  # Get the current PANEL from grouping
-      scales <- panel_scales(pieces$panel, current_panel)
+      scales <- panel_scales(pieces$panel, PANEL)
       details <- plot$coordinates$train(scales)
       as.data.table(plot$coordinates$transform(as.data.frame(.SD), details))
     }, by = PANEL]
@@ -25,7 +22,7 @@ pranges <- function(plot) {
 
   x_ranges <- lapply(panels$x_scales, function(scale) scale$get_limits())
   y_ranges <- lapply(panels$y_scales, function(scale) scale$get_limits())
-  
+
   npscales <- plot$scales$non_position_scales()
   npranges <- lapply(npscales$scales$scales, function(scale) scale$get_limits())
 
