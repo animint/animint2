bd09b21e1b9c65e1dc14ae5e5e7bc6b120daeee6
Suhaani Agarwal <suhaani0707@gmail.com>
2025-06-07 19:35:19 +0530

    added interactivity test

--- tests/testthat/test-renderer3-tooltip-interactivity.R
+++ tests/testthat/test-renderer3-tooltip-interactivity.R
@@ -3,7 +3,7 @@ acontext("tooltip-interactivity")
 data("CO2")
 
 plot_viz <- ggplot() + 
-  geom_point(aes(conc, uptake, color=Treatment, tooltip=Plant),
+  geom_point(aes(conc, uptake, color=Treatment, tooltip=Plant , id = paste0(CO2$Plant, "_", seq_len(nrow(CO2)))), # Unique ID for every circle
              data = CO2)+
   geom_rect(aes(xmin=100, xmax=200, ymin=20, ymax=30, 
               tooltip="Test Rectangle"), alpha=0.5)
@@ -31,7 +31,41 @@ test_that("tooltip shows correct content on hover interaction", {
   expect_equal(xmlValue(tooltip_div), "Test Rectangle")
   # Simulate mouseout
   remDr$Input$dispatchMouseEvent(type = "mouseMoved", x = 0, y = 0)
-  clickID("background")
+})
+
+test_that("Interactivity does not mess up tooltips", {
+  # Hide all nonchilled points
+  clickID("plot_p_Treatment_variable_nonchilled")
+  # Hover over a chilled point
+  bbox_chilled <- get_element_bbox('circle#Qc1_22')
+  remDr$Input$dispatchMouseEvent(
+    type = "mouseMoved",
+    x = bbox_chilled$center_x,
+    y = bbox_chilled$center_y
+  )
+  Sys.sleep(0.2)
+  tooltip_div <- getNodeSet(getHTML(), '//div[@class="animint-tooltip"]')[[1]]
+  tooltip_text <- xmlValue(tooltip_div)
+  expect_match(tooltip_text, "Qc1", info = "Expected tooltip for chilled point Qc1")
+
+  # show nonchilled points
+  clickID("plot_p_Treatment_variable_nonchilled")
+  Sys.sleep(0.2)
+
+  # Hover over a nonchilled point
+  bbox_nonchilled <- get_element_bbox('circle#Qn1_3')
+  remDr$Input$dispatchMouseEvent(
+    type = "mouseMoved",
+    x = bbox_nonchilled$center_x,
+    y = bbox_nonchilled$center_y
+  )
+  Sys.sleep(0.2)
+  tooltip_div <- getNodeSet(getHTML(), '//div[@class="animint-tooltip"]')[[1]]
+  tooltip_text <- xmlValue(tooltip_div)
+  expect_match(tooltip_text, "Qn1", info = "Expected tooltip for nonchilled point Qn1")
+
+  # Move mouse away at the end
+  remDr$Input$dispatchMouseEvent(type = "mouseMoved", x = 0, y = 0)
 })
 
 test_that("tooltip is hidden after mouseout", {
