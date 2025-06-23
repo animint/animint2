b94919c261bad5b980a7644e482e8d305c827a26
Suhaani Agarwal <suhaani0707@gmail.com>
2025-06-23 23:10:37 +0530

    testing only r coverage

--- tests/testthat.R
+++ tests/testthat.R
@@ -14,10 +14,8 @@ if(filter == ""){
 message(gh.action)
 if(use.browser) {
   tests_init()
-  start_js_coverage() # <-- Start JS coverage
 }
 tests_run(filter=filter)
 if(use.browser) {
-  stop_js_coverage("../js-coverage.json") # <-- Save JS coverage
   tests_exit()
 }
