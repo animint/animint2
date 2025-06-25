82677794600f61b72cb3d77ce97679cffffb6213
Suhaani Agarwal <suhaani0707@gmail.com>
2025-06-25 18:16:29 +0530

    added js coverage collection

--- tests/testthat.R
+++ tests/testthat.R
@@ -8,14 +8,21 @@ source("helper-plot-data.r")
 filter <- Sys.getenv("TEST_SUITE")
 gh.action <- Sys.getenv("GH_ACTION")
 use.browser <- grepl("renderer", filter)
+collect.coverage <- Sys.getenv("COLLECT_COVERAGE", "FALSE") == "TRUE"
 if(filter == ""){
   filter <- NULL
 }
 message(gh.action)
 if(use.browser) {
   tests_init()
+  if(collect.coverage) {
+    start_js_coverage()
+  }
 }
 tests_run(filter=filter)
 if(use.browser) {
+  if(collect.coverage) {
+    stop_js_coverage()
+  }
   tests_exit()
 }
