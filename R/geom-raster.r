760a8f98da76335255dd8aa680f94cd6edea6202
Biplab Sutradhar <biplabsutradhar1453@gmail.com>
2025-06-29 23:20:32 +0530

    Remove geom_raster and  annotation_raster from DESCRIPTION, NAMESPACE, and documentation

--- R/geom-raster.r
+++
@@ -1,5 +0,0 @@
-#' @title geom_raster
-#' @description This function is not supported in animint2. Use `geom_tile` instead.
-geom_raster <- function(...) {
-  stop("geom_raster is not supported in animint2. Please use geom_tile instead.")
-}
