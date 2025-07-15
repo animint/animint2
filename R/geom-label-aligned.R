49fe01a1eaf2a1314e18d7749bd2faad47319271
Toby Dylan Hocking <toby.hocking@r-project.org>
2025-07-15 09:12:40 -0400

    fix example and white space

--- R/geom-label-aligned.R
+++ R/geom-label-aligned.R
@@ -26,47 +26,48 @@
 #' @param background_rect Disables text background rect if set to FALSE.
 #' @export
 #' @examples
-#' # Example 1:
-#' df <- data.frame(
-#'   x = c(1, 2, 3),
-#'   y = c(1, 2, 3),
-#'   label = c("A", "B", "C")
-#' )
-#' geom_label_aligned(data = df, aes(x, y, label = label))
-#' 
-#' # Example 2:
-#' 
 #' library(nlme)
-#' library(dplyr)
 #' data(BodyWeight, package = "nlme")
 #' # Extracting the last point of each rat's trajectory
-#' label_data <- BodyWeight %>%
-#'   group_by(Rat) %>%
-#'   filter(Time == max(Time)) %>%
-#'   ungroup() %>%
-#'   mutate(label = as.character(Rat))
-#' viz <- list(
+#' library(data.table)
+#' label_data <- data.table(BodyWeight)[Time == max(Time)]
+#' library(animint2)
+#' viz <- animint(
 #'   bodyPlot = ggplot() +
-#'     geom_line(aes(x = Time, y = weight, group = Rat, colour = Rat),
-#'               data = BodyWeight) +
-#'     geom_label_aligned(aes(x = Time, y = weight, label = label, fill = Rat),
-#'                        data = label_data) +
-#'     facet_wrap(~Diet, nrow = 1) +
+#'     theme_bw() +
+#'     theme_animint(width=800)+
+#'     geom_line(aes(
+#'       x = Time, y = weight, group = Rat),
+#'       clickSelects="Rat",
+#'       size=3,
+#'       data = BodyWeight) +
+#'     geom_line(aes(
+#'       x = Time, y = weight, group = Rat, colour = Rat),
+#'       clickSelects="Rat",
+#'       data = BodyWeight) +
+#'     geom_label_aligned(aes(
+#'       x = Time, y = weight, label = Rat, fill = Rat),
+#'       clickSelects="Rat",
+#'       hjust = 0,
+#'       data = label_data) +
+#'     facet_grid(~Diet) +
 #'     ggtitle("Rat body weight over time by diet") +
 #'     xlab("Time (days)") +
 #'     ylab("Body Weight (grams)")
 #' )
-geom_label_aligned <- function(mapping = NULL, data = NULL,
-                              stat = "identity", position = "identity",
-                              ...,
-                              label.r = unit(0.15, "lines"),
-                              label.size = 0.25,
-                              alignment = "vertical",
-                              min_distance = 0.1,
-                              background_rect = TRUE,
-                              na.rm = FALSE,
-                              show.legend = NA,
-                              inherit.aes = TRUE) {
+#' viz
+geom_label_aligned <- function
+(mapping = NULL, data = NULL,
+  stat = "identity", position = "identity",
+  ...,
+  label.r = unit(0.15, "lines"),
+  label.size = 0.25,
+  alignment = "vertical",
+  min_distance = 0.1,
+  background_rect = TRUE,
+  na.rm = FALSE,
+  show.legend = NA,
+  inherit.aes = TRUE) {
   layer(
     data = data,
     mapping = mapping,
@@ -91,34 +92,30 @@ geom_label_aligned <- function(mapping = NULL, data = NULL,
 #' @format NULL
 #' @usage NULL
 #' @export
-GeomLabelAligned <- gganimintproto("GeomLabelAligned", Geom,
+GeomLabelAligned <- gganimintproto(
+  "GeomLabelAligned",
+  Geom,
   required_aes = c("x", "y", "label"),
-  
   default_aes = aes(
     colour = "black", fill = "white", size = 3.88, 
     angle = 0, hjust = 0.5, vjust = 0.5, alpha = 1,
     family = "", fontface = 1, lineheight = 1.2, fontsize = 12
   ),
-
-  draw_panel = function(self, data, panel_scales, coord,
-                       label.r = unit(0.15, "lines"),
-                       label.size = 0.25,
-                       alignment = "vertical",
-                       min_distance = 0.1,
-                       background_rect = TRUE,
-                       na.rm = FALSE) {
-    
+  draw_panel = function
+  (self, data, panel_scales, coord,
+    label.r = unit(0.15, "lines"),
+    label.size = 0.25,
+    alignment = "vertical",
+    min_distance = 0.1,
+    background_rect = TRUE,
+    na.rm = FALSE) {
     if (empty(data)) return(zeroGrob())
-
     coords <- coord$transform(data, panel_scales)
-
     coords$label.r <- convertWidth(label.r, "native", valueOnly = TRUE)
     coords$label.size <- label.size
     coords$alignment <- alignment
     coords$min_distance <- min_distance
     coords$background_rect <- background_rect
-
-
     rect_grobs <- lapply(1:nrow(coords), function(i) {
       grid::roundrectGrob(
         x = unit(coords$x[i], "native"),
@@ -134,7 +131,6 @@ GeomLabelAligned <- gganimintproto("GeomLabelAligned", Geom,
         )
       )
     })
-
     text_grobs <- lapply(1:nrow(coords), function(i) {
       grid::textGrob(
         coords$label[i],
@@ -150,18 +146,16 @@ GeomLabelAligned <- gganimintproto("GeomLabelAligned", Geom,
         )
       )
     })
-
-    grobs <- mapply(function(r, t) grid::gTree(children = grid::gList(r, t)), 
-                   rect_grobs, text_grobs)
+    grobs <- mapply(
+      function(r, t) grid::gTree(children = grid::gList(r, t)),
+      rect_grobs, text_grobs)
     class(grobs) <- "gList"
     ggname("geom_label_aligned", grid::grobTree(children = grobs))
   },
-
-   pre_process = function(g, g.data, ...) {
+  pre_process = function(g, g.data, ...) {
     # This ensures our geom is identified as "label_aligned" in JS
     g$geom <- "label_aligned"
     return(list(g = g, g.data = g.data))
   },
-  
   draw_key = draw_key_label
 )
