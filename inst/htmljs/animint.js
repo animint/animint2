e55d70698504c729560d2f6d34a77a77a7b04ad7
Suhaani Agarwal <suhaani0707@gmail.com>
2025-08-20 12:04:03 +0530

    removed duplicate logic in animint.js by adding helper fns

--- inst/htmljs/animint.js
+++ inst/htmljs/animint.js
@@ -1356,42 +1356,46 @@ var animint = function (to_select, json_file) {
       data_to_bind = data;
       if (g_info.geom == "segment") {
   g_info.style_list = line_style_list;
+  // helper: get slope and intercept for datum
+  function getLineParams(d) {
+    var slope = d.slope !== undefined ? +d.slope :
+      (g_info.abline_params && g_info.abline_params.slopes) ? g_info.abline_params.slopes[d.i] : 1;
+    var intercept = d.intercept !== undefined ? +d.intercept :
+      (g_info.abline_params && g_info.abline_params.intercepts) ? g_info.abline_params.intercepts[d.i] : 0;
+    return { slope, intercept };
+  }
+  // helper: compute clipped x for a given y
+  function clipX(y, slope, intercept, xDomain) {
+    return (y - intercept) / slope;
+  }
+  // helper: compute abline endpoints
+  function getAblineCoords(d, scales) {
+    var { slope, intercept } = getLineParams(d);
+    var xDomain = scales.x.domain();
+    var yDomain = scales.y.domain();
+    // x1,y1
+    var x1 = xDomain[0];
+    var y1 = slope * x1 + intercept;
+    if (y1 < yDomain[0]) x1 = clipX(yDomain[0], slope, intercept, xDomain);
+    if (y1 > yDomain[1]) x1 = clipX(yDomain[1], slope, intercept, xDomain);
+    // x2,y2
+    var x2 = xDomain[1];
+    var y2 = slope * x2 + intercept;
+    if (y2 < yDomain[0]) x2 = clipX(yDomain[0], slope, intercept, xDomain);
+    if (y2 > yDomain[1]) x2 = clipX(yDomain[1], slope, intercept, xDomain);
+    return {
+      x1: scales.x(x1),
+      y1: scales.y(Math.max(yDomain[0], Math.min(yDomain[1], slope * xDomain[0] + intercept))),
+      x2: scales.x(x2),
+      y2: scales.y(Math.max(yDomain[0], Math.min(yDomain[1], slope * xDomain[1] + intercept)))
+    };
+  }
   if (g_info.is_abline) {
     eActions = function(e) {
-      e.attr("x1", function(d){
-        var slope = d.slope !== undefined ? +d.slope : (g_info.abline_params && g_info.abline_params.slopes) ? g_info.abline_params.slopes[d.i] : 1;
-        var intercept = d.intercept !== undefined ? +d.intercept : (g_info.abline_params && g_info.abline_params.intercepts) ? g_info.abline_params.intercepts[d.i] : 0;
-        var xDomain = scales.x.domain();
-        var yDomain = scales.y.domain();
-        var x1 = xDomain[0];
-        var y1 = slope * x1 + intercept;
-        if (y1 < yDomain[0]) x1 = (yDomain[0] - intercept) / slope;
-        if (y1 > yDomain[1]) x1 = (yDomain[1] - intercept) / slope;        
-        return scales.x(x1);
-      })
-      .attr("x2", function(d) {
-        var slope = d.slope !== undefined ? +d.slope : (g_info.abline_params && g_info.abline_params.slopes) ? g_info.abline_params.slopes[d.i] : 1;
-        var intercept = d.intercept !== undefined ? +d.intercept : (g_info.abline_params && g_info.abline_params.intercepts) ? g_info.abline_params.intercepts[d.i] : 0;
-        var xDomain = scales.x.domain();
-        var yDomain = scales.y.domain();
-        var x2 = xDomain[1];
-        var y2 = slope * x2 + intercept;
-        if (y2 < yDomain[0]) x2 = (yDomain[0] - intercept) / slope;
-        if (y2 > yDomain[1]) x2 = (yDomain[1] - intercept) / slope;
-        return scales.x(x2);
-      })
-      .attr("y1", function(d) {
-        var slope = d.slope !== undefined ? +d.slope : (g_info.abline_params && g_info.abline_params.slopes) ? g_info.abline_params.slopes[d.i] : 1;
-        var intercept = d.intercept !== undefined ? +d.intercept : (g_info.abline_params && g_info.abline_params.intercepts) ? g_info.abline_params.intercepts[d.i] : 0;
-        var y1 = slope * scales.x.domain()[0] + intercept;
-        return scales.y(Math.max(scales.y.domain()[0], Math.min(scales.y.domain()[1], y1)));
-      })
-      .attr("y2", function(d) {
-        var slope = d.slope !== undefined ? +d.slope : (g_info.abline_params && g_info.abline_params.slopes) ? g_info.abline_params.slopes[d.i] : 1;
-        var intercept = d.intercept !== undefined ? +d.intercept : (g_info.abline_params && g_info.abline_params.intercepts) ? g_info.abline_params.intercepts[d.i] : 0;
-        var y2 = slope * scales.x.domain()[1] + intercept;
-        return scales.y(Math.max(scales.y.domain()[0], Math.min(scales.y.domain()[1], y2)));
-      });
+      e.attr("x1", d => getAblineCoords(d, scales).x1)
+       .attr("y1", d => getAblineCoords(d, scales).y1)
+       .attr("x2", d => getAblineCoords(d, scales).x2)
+       .attr("y2", d => getAblineCoords(d, scales).y2);
     };
   } else {
     // Regular segment case
