92d6971eb5d3ee0aa5d9a1d758e6879ab7e1cc3b
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2025-09-02 15:53:24 -0400

    fix warnings

--- tests/testthat/test-compiler-plot-named-timexxx.R
+++ tests/testthat/test-compiler-plot-named-timexxx.R
@@ -4,26 +4,27 @@ data(WorldBank, package = "animint2")
 not.na <- subset(WorldBank, !(is.na(life.expectancy) | is.na(fertility.rate)))
 subset(not.na, is.na(not.na$population))
 not.na[not.na$country=="Kuwait", "population"] <- 1700000
-viz <-
-  list(scatter=ggplot()+
-         geom_point(aes(life.expectancy, fertility.rate,
-                        colour=region, size=population, key=year),
-                    clickSelects="country",
-                    showSelected="year",
-                    data=not.na)+
-       geom_text(aes(life.expectancy, fertility.rate, label=country),
-                 data=not.na,
-                     showSelected=c("country", "year"))+
-       scale_size_animint(breaks=10^(5:9))+
-       make_text(WorldBank, 55, 9, "year"),
-
-       timeSeries=ggplot()+
-       make_tallrect(WorldBank, "year")+
-       geom_line(aes(year, life.expectancy, group=country, colour=region),
-                 data=WorldBank, size=4, alpha=3/5,
-                     clickSelects="country"),
-
-       duration=list(year=1000))
+viz <- animint(
+  scatter=ggplot()+
+    geom_point(aes(
+      life.expectancy, fertility.rate,
+      colour=region, size=population, key=year),
+      clickSelects="country",
+      showSelected="year",
+      data=not.na)+
+    geom_text(aes(
+      life.expectancy, fertility.rate, label=country, key=country),
+      data=not.na,
+      showSelected=c("country", "year"))+
+    scale_size_animint(breaks=10^(5:9))+
+    make_text(WorldBank, 55, 9, "year"),
+  timeSeries=ggplot()+
+    make_tallrect(WorldBank, "year")+
+    geom_line(aes(
+      year, life.expectancy, group=country, colour=region),
+      data=WorldBank, size=4, alpha=3/5,
+      clickSelects="country"),
+  duration=list(year=1000))
 
 test_that("plot named timeSeries is OK without time option list", {
   meta <- animint2dir(viz, open.browser=FALSE)
@@ -38,26 +39,27 @@ test_that("plot named timeSeries is OK with time option list", {
   expect_is(meta, "environment")
 })
 
-bad <-
-  list(scatter=ggplot()+
-         geom_point(aes(life.expectancy, fertility.rate,
-                        colour=region, size=population),
-                    clickSelects="country",
-                      showSelected="year", 
-                  data=not.na)+
-       geom_text(aes(life.expectancy, fertility.rate, label=country),
-                 data=not.na,
-                     showSelected=c("country", "year"))+
-       scale_size_animint(breaks=10^(5:9))+
-       make_text(WorldBank, 55, 9, "year"),
-
-       time=ggplot()+
-       make_tallrect(WorldBank, "year")+
-       geom_line(aes(year, life.expectancy, group=country, colour=region),
-                 data=WorldBank, size=4, alpha=3/5,
-                     clickSelects="country"),
-
-       duration=list(year=1000))
+bad <- animint(
+  scatter=ggplot()+
+    geom_point(aes(
+      life.expectancy, fertility.rate,
+      colour=region, size=population),
+      clickSelects="country",
+      showSelected="year",
+      data=not.na)+
+    geom_text(aes(
+      life.expectancy, fertility.rate, label=country),
+      data=not.na,
+      showSelected=c("country", "year"))+
+    scale_size_animint(breaks=10^(5:9))+
+    make_text(WorldBank, 55, 9, "year"),
+  time=ggplot()+
+    make_tallrect(WorldBank, "year")+
+    geom_line(aes(
+      year, life.expectancy, group=country, colour=region),
+      data=WorldBank, size=4, alpha=3/5,
+      clickSelects="country"),
+  duration=list(year=1000))
 
 test_that("plot named time is NOT OK", {
   expect_error({
