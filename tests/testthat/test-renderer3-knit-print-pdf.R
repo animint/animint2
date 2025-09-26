27012f9eee5e2d3fd530495525054976931ba76b
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2025-09-26 11:37:46 -0400

    test animint output to tex

---
+++ tests/testthat/test-renderer3-knit-print-pdf.R
@@ -0,0 +1,26 @@
+acontext("render animint in pdf")
+knitr::knit_meta() #clear knitr 'metadata'
+screenshot.Rmd <- system.file(
+  "examples", "test_knit_print_screenshot.Rmd",
+  package = "animint2",
+  mustWork = TRUE)
+folder <- file.path("animint-htmltest", "test-pdf-folder")
+unlink(folder, recursive = TRUE)
+doc.Rmd <- file.path(folder, "doc.Rmd")
+dir.create(folder, recursive = TRUE, showWarnings = FALSE)
+file.copy(screenshot.Rmd, doc.Rmd, overwrite=TRUE)
+rmarkdown::render(doc.Rmd)
+Capture.PNG <- file.path(folder, "unnamedchunk1", "Capture.PNG")
+
+test_that("PNG and PDF files exist", {
+  expect_gt(file.size(Capture.PNG), 0)
+  doc.pdf <- file.path(folder, "doc.pdf")
+  expect_gt(file.size(doc.pdf), 0)
+})
+
+test_that("tex file contains include", {
+  doc.tex <- file.path(folder, "doc.tex")
+  doc.lines <- readLines(doc.tex)
+  include <- grep("includegraphics[width=\\textwidth]", doc.lines, fixed=TRUE, value=TRUE)
+  expect_match(include, Capture.PNG, fixed=TRUE)
+})
