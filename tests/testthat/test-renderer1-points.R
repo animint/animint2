4c06fd75580e126e5868cfc273f62f7e0d5fb7c5
Gauarv Chaudhary <chaudharygaurav2004@gmail.com>
2025-10-28 20:39:05 +0530

    Made all the changes in the tets file as asked

--- tests/testthat/test-renderer1-points.R
+++ tests/testthat/test-renderer1-points.R
@@ -3,12 +3,6 @@ test_that("geom_point warns for shape != 21", {
     animint(p = ggplot() + geom_point(aes(Sepal.Length, Sepal.Width), data=iris, shape=1)),
     "shape.*21"
   )
-  
-  expect_warning(
-    animint(p = ggplot() + geom_point(aes(Sepal.Length, Sepal.Width, shape=Species), data=iris)),
-    "shape.*21"
-  )
-  
   expect_silent(
     animint(p = ggplot() + geom_point(aes(Sepal.Length, Sepal.Width), data=iris, shape=21))
   )
