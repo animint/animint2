0f960ca6c5a4a4504d3b0108f71a49ea79eaea57
Gauarv Chaudhary <chaudharygaurav2004@gmail.com>
2025-11-27 20:48:51 +0530

    fix: use correct get_element_bbox signature in multiline tests

--- tests/testthat/test-renderer1-multiline-spacing.R
+++ tests/testthat/test-renderer1-multiline-spacing.R
@@ -9,13 +9,13 @@ viz <- list(
 )
 info <- animint2HTML(viz)
 test_that("multiline plot title with large font does not overlap plot area", {
-  title_bbox <- get_element_bbox(info$html, '//text[@class="plottitle"]')
-  plot_rect <- get_element_bbox(info$html, '//svg[@id="plot_plot1"]//rect[@class="plot_rect"]')
-  expect_lt(title_bbox$bottom, plot_rect$top)
+  title_bbox <- get_element_bbox("text.plottitle")
+  plot_rect <- get_element_bbox("#plot_plot1 rect.background_rect")
+  expect_lt(title_bbox$top + title_bbox$height, plot_rect$top)
 })
 test_that("multiline y-axis title with large font does not overlap plot area", {
-  ytitle_bbox <- get_element_bbox(info$html, '//text[@class="ytitle"]')
-  plot_rect <- get_element_bbox(info$html, '//svg[@id="plot_plot1"]//rect[@class="plot_rect"]')
-  expect_lt(ytitle_bbox$right, plot_rect$left)
+  ytitle_bbox <- get_element_bbox("text.ytitle")
+  plot_rect <- get_element_bbox("#plot_plot1 rect.background_rect")
+  expect_lt(ytitle_bbox$left + ytitle_bbox$width, plot_rect$left)
 })
 
