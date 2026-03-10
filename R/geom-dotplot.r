36fda33ad62398dfc61774e201696eb9e98553ce
NandaniAggarwal <nandaniaggarwal0109@gmail.com>
2026-03-11 00:40:55 +0530

    Refactor geom_dotplot to simplify error message

--- R/geom-dotplot.r
+++ R/geom-dotplot.r
@@ -7,8 +7,5 @@
 #' @export
 #' @keywords internal
 geom_dotplot <- function(...) {
-  stop(
-    "geom_dotplot() has been removed from animint2. Use geom_point() instead.\n",
-    "See issue #289 for details: https://github.com/animint/animint2/issues/289"
-  )
+  stop("geom_dotplot() has been removed from animint2. Use geom_point() instead. See issue #289 for details: https://github.com/animint/animint2/issues/289")
 }
