90d9390798e064fc9274fa1e8f2f192f9c0b1bc9
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2026-05-22 15:05:13 -0400

    3 polygon test

--- tests/testthat/test-renderer-polygon-holes.R
+++ tests/testthat/test-renderer-polygon-holes.R
@@ -65,3 +65,135 @@ test_that("point3_1 is in background (no polygon tooltip)", {
   computed <- tooltipID("point3_1")
   expect_identical(computed$opacity, 0)
 })
+
+## more complex test with 3 polygons.
+m.list <- list(
+  island=rbind(
+    c(0,0,0,0,0,0,0),
+    c(0,1,1,1,1,1,0),
+    c(0,1,0,0,0,1,0),
+    c(0,1,0,1,0,1,0),
+    c(0,1,0,0,0,1,0),
+    c(0,1,1,1,1,1,0),
+    c(0,0,0,0,0,0,0)),
+  hole=rbind(
+    c(0,0,0,0,0,0,0),
+    c(0,1,1,1,1,1,0),
+    c(0,1,0,0,0,1,0),
+    c(0,1,0,0,0,1,0),
+    c(0,1,0,0,0,1,0),
+    c(0,1,1,1,1,1,0),
+    c(0,0,0,0,0,0,0)),
+  filled=rbind(
+    c(0,0,0,0,0,0,0),
+    c(0,1,1,1,1,1,0),
+    c(0,1,1,1,1,1,0),
+    c(0,1,1,1,1,1,0),
+    c(0,1,1,1,1,1,0),
+    c(0,1,1,1,1,1,0),
+    c(0,0,0,0,0,0,0)))
+poly.list  <- list()
+point.list <- list()
+for(grp.i in seq_along(m.list)){
+  offset <- grp.i * 6
+  group <- names(m.list)[[grp.i]]
+  m <- m.list[[grp.i]]
+  clines <- contourLines(
+    1:ncol(m), 1:nrow(m), m, levels=0.5)
+  poly.list[[grp.i]] <- data.table(
+    subgroup=seq_along(clines)
+  )[, with(clines[[subgroup]], data.table(
+    group, x=x+offset, y
+  )), by=subgroup]
+  point.list[[grp.i]] <- data.table(
+    x = offset+4,
+    y = 4:7,
+    id = paste0(group, "_", c("mid","hole","ring","out")))
+}
+poly.dt <- do.call(rbind, poly.list)
+point.dt <- do.call(rbind, point.list)
+
+viz.full <- animint(
+  poly=ggplot()+
+    geom_point(aes(
+      x, y, id=id),
+      data=point.dt,
+      color="red", size=3)+
+    geom_text(aes(
+      x, y, label=id),
+      data=point.dt,
+      hjust=0,
+      color="red", size=20)+
+    geom_polygon(aes(
+      x, y, group=group, subgroup=subgroup, tooltip=group),
+      data=poly.dt,
+      alpha=0.5,
+      fill="steelblue")+
+    theme_animint(width=800, height=300))
+info.full <- animint2HTML(viz.full)
+
+test_that("island_mid is in polygon (yes polygon tooltip)", {
+  computed <- tooltipID("island_mid")
+  expect_identical(computed$opacity, 0.7)
+  expect_identical(computed$value, "island")
+})
+
+test_that("island_hole is not in polygon (no polygon tooltip)", {
+  computed <- tooltipID("island_hole")
+  expect_identical(computed$opacity, 0)
+})
+
+test_that("island_ring is in polygon (yes polygon tooltip)", {
+  computed <- tooltipID("island_ring")
+  expect_identical(computed$opacity, 0.7)
+  expect_identical(computed$value, "island")
+})
+
+test_that("island_out is not in polygon (no polygon tooltip)", {
+  computed <- tooltipID("island_out")
+  expect_identical(computed$opacity, 0)
+})
+
+test_that("hole_mid is not in polygon (no polygon tooltip)", {
+  computed <- tooltipID("hole_mid")
+  expect_identical(computed$opacity, 0)
+})
+
+test_that("hole_hole is not in polygon (no polygon tooltip)", {
+  computed <- tooltipID("hole_hole")
+  expect_identical(computed$opacity, 0)
+})
+
+test_that("hole_ring is in polygon (yes polygon tooltip)", {
+  computed <- tooltipID("hole_ring")
+  expect_identical(computed$opacity, 0.7)
+  expect_identical(computed$value, "hole")
+})
+
+test_that("hole_out is not in polygon (no polygon tooltip)", {
+  computed <- tooltipID("hole_out")
+  expect_identical(computed$opacity, 0)
+})
+
+test_that("filled_mid is in polygon (yes polygon tooltip)", {
+  computed <- tooltipID("filled_mid")
+  expect_identical(computed$opacity, 0.7)
+  expect_identical(computed$value, "filled")
+})
+
+test_that("filled_hole is in polygon (yes polygon tooltip)", {
+  computed <- tooltipID("filled_hole")
+  expect_identical(computed$opacity, 0.7)
+  expect_identical(computed$value, "filled")
+})
+
+test_that("filled_ring is in polygon (yes polygon tooltip)", {
+  computed <- tooltipID("filled_ring")
+  expect_identical(computed$opacity, 0.7)
+  expect_identical(computed$value, "filled")
+})
+
+test_that("filled_out is not in polygon (no polygon tooltip)", {
+  computed <- tooltipID("filled_out")
+  expect_identical(computed$opacity, 0)
+})
