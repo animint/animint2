c2ca989064b247318f72e23aeae9bd7b8eca94d7
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-05-23 07:49:11 -0400

    remove simple test

--- tests/testthat/test-renderer-polygon-holes.R
+++ tests/testthat/test-renderer-polygon-holes.R
@@ -1,59 +1,6 @@
 acontext("Polygon holes via subgroup aesthetic")
 library(data.table)
 
-m <- matrix(c(
-  0,0,0,0,0,0,
-  0,1,1,1,1,0,
-  0,1,0,0,1,0,
-  0,1,0,0,1,0,
-  0,1,1,1,1,0,
-  0,0,0,0,0,0
-), 6, 6, byrow=TRUE)
-clines <- contourLines(
-  1:ncol(m), 1:nrow(m),
-  m, levels=0.5)
-simple.polygon.dt <- data.table(subgroup=seq_along(clines))[
-, clines[[subgroup]][c("x","y")]
-, by=subgroup][, group := 1][]
-simple.point.dt <- data.table(
-  row=as.integer(row(m)),
-  col=as.integer(col(m)),
-  value=as.numeric(m)
-)[, id := sprintf("point%d_%d", col, row)][]
-viz.simple <- animint(
-  selector.types=list(group="multiple"),
-  poly=ggplot()+
-    geom_point(aes(
-      col, row, fill=value, id=id),
-      size=5,
-      data=simple.point.dt)+
-    geom_text(aes(
-      col, row, label=id),
-      data=simple.point.dt)+
-    geom_polygon(
-      aes(x, y, group=group, subgroup=subgroup, tooltip=paste("group", group)),
-      data=simple.polygon.dt,
-      alpha=0.5,
-      fill="red"))
-info <- animint2HTML(viz.simple)
-
-test_that("point3_3 is in hole (no polygon tooltip)", {
-  computed <- tooltipID("point3_3")
-  expect_identical(computed$opacity, 0)
-})
-
-test_that("point3_2 is in polygon (yes polygon tooltip)", {
-  computed <- tooltipID("point3_2")
-  expect_identical(computed$opacity, 0.7)
-  expect_identical(computed$value, "group 1")
-})
-
-test_that("point3_1 is in background (no polygon tooltip)", {
-  computed <- tooltipID("point3_1")
-  expect_identical(computed$opacity, 0)
-})
-
-## more complex test with 3 polygons.
 m.list <- list(
   island=rbind(
     c(0,0,0,0,0,0,0),
@@ -116,7 +63,7 @@ viz.full <- animint(
     theme_animint(width=800, height=300))
 info.full <- animint2HTML(viz.full)
 
-test_that("island_mid is in polygon (yes polygon tooltip)", {
+test_that("polygon tooltip only shown for point with num=1", {
   computed.dt.list <- list()
   for(point.i in 1:nrow(point.dt)){
     point.row <- point.dt[point.i]
