df17593c7c4c397840c622ec298b961fbcbc795a
Gaurav Chaudhary <chaudharygaurav2004@gmail.com>
2026-06-11 08:15:28 +0530

    Remove test-renderer3-chromote-timeout.R feedback I got from tdhock do not add tests for test-helper functions.

--- tests/testthat/test-renderer3-chromote-timeout.R
+++
@@ -1,9 +0,0 @@
-acontext("chromote timeout")
-
-test_that("animint2HTML uses a safe page reload timeout", {
-  remDr$default_timeout <- 1
-  viz <- list(p=ggplot()+geom_point(aes(1, 1)))
-  info <- animint2HTML(viz)
-  expect_equal(remDr$default_timeout, 30)
-  expect_s3_class(info$html, "XMLInternalDocument")
-})
