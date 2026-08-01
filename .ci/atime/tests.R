c522b8e1f540843b94c7c712c6a8afca8954362b
nishita-shah1 <nishita182005@gmail.com>
2026-08-01 23:29:10 +0530

    atime: point Fast to tip of PR #342

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -10,7 +10,7 @@ test.list <- atime::atime_test_list(
     },
     seconds.limit=1,
     Slow="352f7e10040cb9de6ddd16416d342e9746c14c7a", # Parent of the first commit (https://github.com/animint/animint2/commit/121a11399e7d6ca6c822cd22472886c6d4d8cf10) of the PR (https://github.com/animint/animint2/pull/238/commits).
-    Fast="30950779702e6c8aeecd24aeb737c9fa5ce898e0"), # Last commit in the PR (https://github.com/animint/animint2/pull/238/commits).
+    Fast="188cb3422ee0c87d9a71c06ab42a67b9dccb4be0"), # Last commit in the PR (https://github.com/animint/animint2/pull/238/commits).
   ## Post-#242 workload with NA groups and multiple common columns (#258).
   ## Slow = master before C++ (parent of first C++ commit on PR #342).
   ## Fast = tip of PR #342 with common_value_for_group_subset_cpp().
@@ -29,5 +29,5 @@ test.list <- atime::atime_test_list(
     },
     seconds.limit=2,
     Slow="9dce8611495357d4441793b9494bbce11fcc1a9f", # Parent of first C++ commit https://github.com/animint/animint2/commit/623545cb
-    Fast="5cc3f80a33755cbaa4cb10c21bf868947b112178") # CRAN-NOTES fix on PR https://github.com/animint/animint2/pull/342
+    Fast="188cb3422ee0c87d9a71c06ab42a67b9dccb4be0") # CRAN-NOTES fix on PR https://github.com/animint/animint2/pull/342
 )
