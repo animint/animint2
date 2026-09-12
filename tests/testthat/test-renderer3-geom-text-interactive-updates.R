4366257968812e32a4136a18ecd688fda06d08ce
Gaurav Chaudhary <chaudharygaurav2004@gmail.com>
2026-09-12 20:51:34 +0530

    test: compare geom_text update with static reference (#345)
    Replace 50px threshold with equality against a non-showSelected
    geom_text at the target x-position, per review feedback.
    
    Signed-off-by: Gaurav Chaudhary <chaudharygaurav2004@gmail.com>

--- tests/testthat/test-renderer3-geom-text-interactive-updates.R
+++ tests/testthat/test-renderer3-geom-text-interactive-updates.R
@@ -6,6 +6,10 @@ text.df <- data.frame(
   y = 1,
   label = "marker",
   key = "label")
+reference.df <- data.frame(
+  x = 9,
+  y = 2,
+  label = "marker")
 rect.df <- data.frame(
   item = c("a", "b"),
   xmin = c(0.6, 8.6),
@@ -13,21 +17,27 @@ rect.df <- data.frame(
   id = c("select_a", "select_b"))
 viz <- animint(
   plot1 = ggplot() +
-    theme_animint(width = 400) +
-    scale_x_continuous(limits = c(0, 10)) +
     geom_text(aes(x, y, label = label, key = key),
               showSelected = "item", data = text.df) +
+    geom_text(aes(x, y, label = label), data = reference.df) +
     geom_tallrect(aes(xmin = xmin, xmax = xmax, id = id),
                   clickSelects = "item", data = rect.df),
   duration = list(item = 500),
   first = list(item = "a"))
 info <- animint2HTML(viz)
-get_geom_text_center_x <- function() {
+get_interactive_text_x <- function() {
   get_element_bbox("svg#plot_plot1 g.geom1_text_plot1 text.geom")$center_x
 }
-x_start <- get_geom_text_center_x()
+get_reference_text_x <- function() {
+  get_element_bbox("svg#plot_plot1 g.geom2_text_plot1 text.geom")$center_x
+}
+reference_x <- get_reference_text_x()
+interactive_x <- get_interactive_text_x()
+test_that("interactive geom_text starts left of reference", {
+  expect_lt(interactive_x, reference_x)
+})
 clickID("select_b")
 Sys.sleep(1)
-test_that("geom_text x position updates after showSelected changes", {
-  expect_gt(get_geom_text_center_x() - x_start, 50)
+test_that("geom_text x position matches non-interactive geom after update", {
+  expect_equal(get_interactive_text_x(), reference_x)
 })
